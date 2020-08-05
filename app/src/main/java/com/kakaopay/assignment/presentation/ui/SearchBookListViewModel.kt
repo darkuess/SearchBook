a1aacc89653ba3119f9b1218ca009685334d059c
darkuess <darkuess@gmail.com>
2020-08-05 18:16:10 +0900

    도서명 변경시 자동 검색 기능 추가

--- app/src/main/java/com/kakaopay/assignment/presentation/ui/SearchBookListViewModel.kt
+++ app/src/main/java/com/kakaopay/assignment/presentation/ui/SearchBookListViewModel.kt
@@ -1,18 +1,25 @@
 package com.kakaopay.assignment.presentation.ui
 
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.lifecycle.MutableLiveData
 import androidx.paging.PagingData
 import androidx.paging.rxjava2.cachedIn
+import com.jakewharton.rxrelay2.PublishRelay
 import com.kakaopay.assignment.data.entity.SearchOption
 import com.kakaopay.assignment.domain.entity.BookEntity
 import com.kakaopay.assignment.domain.usecase.SearchUseCase
 import com.kakaopay.assignment.presentation.base.AutoDisposeViewModel
+import com.kakaopay.assignment.presentation.util.SimpleTextWatcher
 import com.uber.autodispose.autoDispose
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 import kotlin.coroutines.CoroutineContext
 
 class SearchBookListViewModel(private val searchUseCase: SearchUseCase) :
@@ -28,6 +35,32 @@ class SearchBookListViewModel(private val searchUseCase: SearchUseCase) :
 
     val loadSearchBook = MutableLiveData<PagingData<BookEntity>>()
 
+    private val autoSearchText: PublishRelay<String> = PublishRelay.create()
+    val textWatcher: TextWatcher = object : SimpleTextWatcher() {
+        override fun afterTextChanged(s: Editable) {
+            autoSearchText.accept(s.toString())
+        }
+    }
+
+    private val autoSearchedData: Flowable<PagingData<BookEntity>> =
+        autoSearchText
+            .debounce(300, TimeUnit.MILLISECONDS)
+            .distinctUntilChanged()
+            .filter { keyword -> keyword.isNotEmpty() }
+            .toFlowable(BackpressureStrategy.LATEST)
+            .switchMap { query ->
+                searchUseCase.loadBookList(SearchOption(query = query, size = DEFAULT_PAGE_SIZE))
+                    .subscribeOn(Schedulers.io())
+                    .cachedIn(scope = this)
+            }
+
+    init {
+        autoSearchedData.subscribeOn(Schedulers.io())
+            .cachedIn(scope = this)
+            .autoDispose(this)
+            .subscribe(loadSearchBook::postValue, Timber::e)
+    }
+
     fun requestSearch() {
         triggerSearch.value = true
         val query = searchText.value ?: return
@@ -35,9 +68,7 @@ class SearchBookListViewModel(private val searchUseCase: SearchUseCase) :
             .subscribeOn(Schedulers.io())
             .cachedIn(scope = this)
             .autoDispose(this)
-            .subscribe({
-                loadSearchBook.postValue(it)
-            }, Timber::e)
+            .subscribe(loadSearchBook::postValue, Timber::e)
     }
 
     companion object {
